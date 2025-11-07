ec6473dfad15abc0205965c15833212f0e2783a3
HukBross <140917589+HukBross257@users.noreply.github.com>
2025-11-06 22:50:22 -0300

    Update run_blocklogger.py

--- run_blocklogger.py
+++ run_blocklogger.py
@@ -1,4 +1,8 @@
 from blocklogger import abrir_os_logs, mostrar_logs
 
-with abrir_os_logs() as dados:
-	mostrar_logs(dados)
+def main():
+	with abrir_os_logs() as dados:
+		mostrar_logs(dados)
+		
+if __name__ == '__main__':
+	main()
