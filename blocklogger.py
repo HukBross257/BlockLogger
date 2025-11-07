80650f4a38157f6a9d7b79096794ecca48f7c839
HukBross <140917589+HukBross257@users.noreply.github.com>
2025-11-06 23:28:31 -0300

    Update blocklogger.py

--- blocklogger.py
+++ blocklogger.py
@@ -5,17 +5,22 @@ import requests
 
 server_log = '' # Coloque aqui o caminho para o server.log do seu server
 discord_webhook = ''
+
+
 def abrir_os_logs():
     ''' Aonde ira abrir o arquivo '''
     return open(server_log, 'r')
 
+
 def mostrar_logs(dados):
     ''' Aqui ira pegar os dados dos logs e enviar para o canal do discord'''
+  
     dados.seek(0, 2)  # Vai para o final do arquivo
     while True:
         linhas = dados.readlines()
         linhas = [linha.strip() for linha in linhas]
 
+        
         for linha in linhas:
             try:
                 requests.post(discord_webhook, json={'content': linha})
